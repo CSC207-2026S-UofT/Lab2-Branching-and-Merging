e6d1ca52402b9464198eb4916d4ca9efb2af172f
Benhou <baronma.mail@utoronto.ca>
2026-05-14 19:32:45 -0400

    change

--- src/Polymorphism.java
+++ src/Polymorphism.java
@@ -1,5 +1,5 @@
 public class Polymorphism {
-    // TODO TASK 1: Fix the bugs in this code to pass PolymorphismTest.java
+
 
     /**
      * Returns the role.
@@ -7,7 +7,8 @@ public class Polymorphism {
      * @return the role of the person
      */
     public static String getStudentRole(Person p) {
-        // TODO: right now for student, it is not returning the role to be student.
+        if (p instanceof Student)
+            return p.role;
         return p.role;
     }
 }
